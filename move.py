5c4a8367a206c598de02686a08ac970fabe31ccb
Jason Banh <jbanh2@gmail.com>
2022-03-06 18:24:30 -0800

    Added some notes, imported BoardTile Enum and laid a basic validate for SingleMove.

--- move.py
+++ move.py
@@ -4,11 +4,38 @@ Contains the various move classes.
 NOTE: Abalone move types:
 in-line: marbles are moved as a column
 sidestep: marbles are moved sideways (only possible with 2-3 marbles)
+
+Moves:
+(Letter Value, Number Value)
+(1, 1) Up and to the right
+    Ex. B4 -> C5
+(1, 0) Up and to the left
+    Ex. B4 -> C4
+(0, 1) To the right
+    Ex. B4 -> B5
+(0, -1) To the left
+    Ex. B4 -> B3
+(-1, 0) Down to the right
+    Ex. B4 -> A4
+(-1, -1) Down to the left
+    Ex. B4 -> A3
 """
 
 from abc import ABC
 import doctest
+from enum import Enum
 
+
+class BoardTile(Enum):
+    # TODO: When consolidated with the main remove and import BoardTile instead.
+    """
+    Red = White
+    Blue = Black
+    """
+    RED = "\U0001F7E5"
+    BLUE = "\U0001F7E6"
+    EMPTY = "\U0001F7E9"
+    BORDER = ""
 
 class Move(ABC):
     """
@@ -16,6 +43,7 @@ class Move(ABC):
 
     Moves are Singleton callable objects that validate a move and return a list of changed board co-ordinates.
     """
+    # TODO: Make Move classes into singletons. (Maybe)
     __instance = None
 
     def __init__(self):
@@ -31,7 +59,7 @@ class Move(ABC):
         """
         pass
 
-    def validate(self, formation, board):
+    def validate(self, formation, board: dict):
         """
         Method to be overridden depending on the type of move.
 
@@ -76,3 +104,22 @@ class DuoMove(Move):
 
     def validate(self, formation, board):
         pass
+
+
+class SingleMove(Move):
+    """
+    Represents the movement of a single marble.
+    """
+
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def move(self, formation, board):
+        pass
+
+    def validate(self, formation, board):
+        if board.get(formation[1]) == 0:
+            return True
+        else:
+            return False
