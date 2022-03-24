91fb4beeaf63da337d261d2b98d6acfa6d337778
Anson Chan <achan436@my.bcit.ca>
2022-03-23 22:40:47 -0700

    updated

--- ab_search.py
+++ ab_search.py
@@ -14,30 +14,19 @@ class InfiniteValues(enum.IntEnum):
 class Search:
     def __init__(self, state):
         self.state = state  # State is a board object
-        self.looks = 0
+        self.starting_depth = 3
         self.alpha = InfiniteValues.NEG_INF
         self.beta = InfiniteValues.POS_INF
         self.dict = {}
-
-    def evaluate_board(self, state, color):
-        black = 0
-        white = 0
-
-        for key, value in state.board.items():
-            if value == BoardTile.BLUE:
-                black += 1
-            if value == BoardTile.RED:
-                white += 1
-
-        return black if color == BoardTile.BLUE else white
 
     def terminal_test(self):
         return False
 
     def ab_search(self):
-        depth = 1
+        depth = 3
         value = self.max_value(self.state, depth)
         print("Overall:", value)
+        print(self.dict)
         return value
 
     def max_value(self, state, depth):
@@ -51,10 +40,7 @@ class Search:
 
         for action in valid_moves:  # Generate list of all possible moves from current state
             new_state = state.get_board_after_move(action)
-            self.looks += 1
             value = max(value, self.min_value(new_state, depth - 1))
-
-            self.dict[value] = action
 
             if value > self.beta:
                 return value
@@ -72,7 +58,6 @@ class Search:
 
         for action in valid_moves:  # Generate list of all possible moves from current state
             new_state = state.get_board_after_move(action)
-            self.looks += 1
             value = min(value, self.max_value(new_state, depth - 1))
 
             if value < self.alpha:
@@ -89,8 +74,6 @@ def main():
     ab_search = Search(board)
     val = ab_search.ab_search()
     board.print_board()
-
-    print(ab_search.dict)
 
 
 
