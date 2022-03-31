632872277376e9400108738687415edcf46d5303
Samuel Cheon <samuelcalling@gmail.com>
2022-03-31 08:43:19 -0700

    adding a few getters and setters to player class

--- player.py
+++ player.py
@@ -8,6 +8,14 @@ class Player(abc.ABC):
         self.turn_limit = turn_time_limit
 
     @abc.abstractmethod
+    def get_move_remaining(self):
+        return self.move_remaining
+
+    @abc.abstractmethod
+    def get_turn_limit(self):
+        return self.turn_limit
+
+    @abc.abstractmethod
     def make_move(self, board):
         pass
 
@@ -16,6 +24,12 @@ class AIPlayer(Player):
 
     def make_move(self, board):
         pass
+
+    def get_move_remaining(self):
+        return self.move_remaining
+
+    def get_turn_limit(self):
+        return self.turn_limit
 
     def __str__(self):
         return f"AI Player with {self.move_remaining} moves " \
@@ -27,6 +41,12 @@ class HumanPlayer(Player):
 
     def make_move(self, board):
         pass
+
+    def get_move_remaining(self):
+        return self.move_remaining
+
+    def get_turn_limit(self):
+        return self.turn_limit
 
     def __str__(self):
         return f"Human Player with {self.move_remaining} moves " \
